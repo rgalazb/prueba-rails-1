ef2555049a73e7881e564d6f6764437592d598b6
rgalazb <galaz.becerra@gmail.com>
2018-02-16 12:10:11 -0300

    rest y list terminados

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,10 @@ class TodosController < ApplicationController
     @todos = Todo.all
   end
 
+  def new
+    @todo = Todo.new
+  end
+
   def create
     todo = Todo.new(todo_params)
     if todo.save
@@ -10,6 +14,42 @@ class TodosController < ApplicationController
     else
       redirect_to todos_path, notice: 'error'
     end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to todos_path, notice: 'exito'
+    else
+      redirect_to edit_todo_path(@post), notice: 'intenta denuevo'
+    end
+  end
+
+  def delete
+    @todo = Todo.find(params[:id])
+    @todo.delete
+
+    redirect_to todos_path
+  end
+
+  def completed
+    @todo = Todo.find(params[:id])
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def list
+    @terminados = Todo.where(completed:true)
+    @sin_terminar = Todo.where(completed:false)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/todos', to: 'todos#index'
-  post '/todos', to: 'todos#create'
+  get 'todos', to: 'todos#index'
+
+  get 'todos/new'
+
+  get 'todos/list'
+
+  post 'todos', to: 'todos#create'
+
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
+
+  patch 'todos/:id', to: 'todos#update'
+
+  delete 'todos/:id', to: 'todos#delete', as: 'delete_todo'
+
+  get 'todos/:id/completed', to: 'todos#completed', as: 'complete_todo'
+
 end
